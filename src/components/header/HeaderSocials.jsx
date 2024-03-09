6c9674c0c6ebccab400ad940fb3d58dbdb145c68
Ranjan Dey <ranjandey700@gmail.com>
2024-03-09 21:00:29 +0000

    modified package json file

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import {BsLinkedin} from 'react-icons/bs'
 import {BsGithub} from 'react-icons/bs'
-import {BsMedium} from 'react-icons/bs'
 
 const HeaderSocials = () => {
   return (
@@ -10,7 +9,7 @@ const HeaderSocials = () => {
 
         <a href="https://www.linkedin.com/in/ranjan-dey/" target="_blank" rel='noreferrer'><BsLinkedin/></a>
         <a href="https://github.com/ranjandey" target="_blank" rel='noreferrer'><BsGithub/></a>
-        {/* <a href="https://medium.com/@thakshiladb2000" target="_blank" rel='noreferrer'><BsMedium/></a> */}
+       
     </div>
   )
 }
